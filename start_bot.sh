1a5ffd84609a8141242c8ecfa3cd95eece1c6ae7
Denis Donskoy <50530415+doublebon@users.noreply.github.com>
2024-01-03 16:39:22 +0300

    Update start_bot.sh

--- start_bot.sh
+++ start_bot.sh
@@ -1,3 +1,3 @@
 cd TelegramAudioBot 
-docker build -t telbot/bot:1 . && docker run --name telbot -itd --restart=always telbot/bot:1
+docker build -t tel-audio-bot/bot:1 . && docker run --name tel-audio-bot -itd --restart=always tel-audio-bot/bot:1
 
